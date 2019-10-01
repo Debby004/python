e7dce292add0003f9766bc58533635d24b2b923d
Debby004 <debby_xia@qq.com>
2019-10-01 12:14:38 +0800

    增加04.py

---
+++ 04.py
@@ -0,0 +1,9 @@
+a = 6
+b = 3
+print('%d + %d = %d' % (a,b,a+b))
+print('%d -  %d = %d' % (a,b,a-b))
+print('%d * %d =%d' % (a,b,a*b))
+print('%d / %d = %f' % (a,b,a/b))
+
+print('%d // %d = %d' % (a,b,a//b))
+print('%d ** %d = %d '%(a,b,a** b ))
