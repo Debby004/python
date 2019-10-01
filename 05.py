ae29b82d05c7706dea26c9e7c662075c2efbe0ec
Debby004 <debby_xia@qq.com>
2019-10-01 17:30:42 +0800

    删除不必要的文件

--- 05.py
+++
@@ -1,7 +0,0 @@
-a = int(input('a='))
-b = int(input('b='))
-a = a+b
-print('a= %d' % (a))
-a *= a+2
-print('a= %d' % (a))
-print('b=%d' % (b))
--- 08.py
+++
@@ -1,8 +0,0 @@
-flag0 = 1==1
-flag1 = 3 > 2
-flag2 = 2 < 1
-flag3 = flag1 and flag2
-flag4 = flag1 or flag2
-flag5 = not (1!=2)
-print('flag0 = ',flag0)
- 
