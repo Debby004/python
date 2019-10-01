ffb785670c6b9ac4c51ea41d4eb06af94f3aa0b7
Debby004 <debby_xia@qq.com>
2019-10-01 16:14:17 +0800

    算术运算符，比较运算符

--- 04.py
+++ 04.py
@@ -1,9 +1,8 @@
-a = 6
-b = 3
+a = int(input('a='))
+b = int(input('b='))
 print('%d + %d = %d' % (a,b,a+b))
 print('%d -  %d = %d' % (a,b,a-b))
 print('%d * %d =%d' % (a,b,a*b))
 print('%d / %d = %f' % (a,b,a/b))
-
 print('%d // %d = %d' % (a,b,a//b))
 print('%d ** %d = %d '%(a,b,a** b ))
---
+++ 05.py
@@ -0,0 +1,7 @@
+a = int(input('a='))
+b = int(input('b='))
+a = a+b
+print('a= %d' % (a))
+a *= a+2
+print('a= %d' % (a))
+print('b=%d' % (b))
